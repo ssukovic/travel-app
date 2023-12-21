fd916583502de4512e8719f3b84798d20e7ce548
aleksandar.stojanovic.ext <aleksandar.stojanovic.ext@adevinta.com>
2023-12-21 11:02:10 +0100

    feat: added modal and location search field

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import SearchModal from "../components/SearchModal";
 import "./HomePage.css";
 
 export const HomePage = () => {
   return (
     <Box className="homepage">
       <Typography variant="h4">Travel experiences</Typography>
-      <Box>Search goes here</Box>
+      <Box>
+        <SearchModal />
+      </Box>
       <Box>
         <Typography variant="h4">Most popular posts</Typography>
       </Box>
