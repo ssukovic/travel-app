106c63fe73fe1a0e6fba17bd71508ba8a3905288
Sara Sukovic <sara.sukovic@productdock.com>
2023-12-21 16:01:27 +0100

    add likes

--- src/views/PostList.js
+++ src/views/PostList.js
@@ -1,19 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { CardMedia } from "@mui/material";
-import {
-  Grid,
-  Avatar,
-  CardActions,
-  Divider,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Grid, CardActions, Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
+import { Likes } from "../components/Likes";
 
 const bull = (
   <Box
@@ -28,23 +22,26 @@ const posts = [
   {
     id: 1,
     title: "A lovely weekend in Paris",
-    cardImg: "https://images.unsplash.com/photo-1431274172761-fca41d930114?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    cardImg:
+      "https://images.unsplash.com/photo-1431274172761-fca41d930114?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     location: "Paris",
-    likes: 23
+    likes: 23,
   },
   {
     id: 2,
     title: "What museums to see in Barcelona!",
     location: "Barcelona",
-    cardImg: "https://images.unsplash.com/photo-1523531294919-4bcd7c65e216?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    likes: 18
+    cardImg:
+      "https://images.unsplash.com/photo-1523531294919-4bcd7c65e216?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    likes: 18,
   },
   {
     id: 3,
     title: "Beaches you have to visit in Lastovo?",
     location: "Lastovo",
-    cardImg: "https://images.unsplash.com/photo-1552568282-59ce97dffb87?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    likes: 21
+    cardImg:
+      "https://images.unsplash.com/photo-1552568282-59ce97dffb87?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    likes: 21,
   },
   // {
   //   id: 4,
@@ -53,7 +50,7 @@ const posts = [
   //   cardImg: "https://images.unsplash.com/photo-1560969184-10fe8719e047?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   //   likes: 4
   // }
-]
+];
 
 export const PostList = () => {
   const navigate = useNavigate();
@@ -69,31 +66,38 @@ export const PostList = () => {
           </Button>
         </CardActions>
       </Grid>
-      {
-        posts.map(p => (
-          <Grid item xs={4}>
-            <Link style={{ textDecoration: 'none' }} to="/detailed-post-view">
-              <Card sx={{ minWidth: 275, height:"100%"}}>
-                <CardMedia
-                  component="img"
-
-                  image={p.cardImg}
-                  alt="Post Images"
-                />
-                <CardHeader mb={0} title={p.title} />
-                <CardContent>
-                  <Typography variant="subtitle2" color="text.secondary" style={{ textAlign: "left" }}>
+      {posts.map((p) => (
+        <Grid item xs={4}>
+          <Link style={{ textDecoration: "none" }} to="/detailed-post-view">
+            <Card sx={{ minWidth: 275, height: "100%" }}>
+              <CardMedia component="img" image={p.cardImg} alt="Post Images" />
+              <CardHeader mb={0} title={p.title} />
+              <CardContent
+                sx={{ display: "flex", justifyContent: "space-between" }}
+              >
+                <div>
+                  <Typography
+                    variant="subtitle2"
+                    color="text.secondary"
+                    style={{ textAlign: "left" }}
+                  >
                     By: Teodora
                   </Typography>
-                  <Typography variant="subtitle2" color="text.secondary" style={{ textAlign: "left" }}>
+                  <Typography
+                    variant="subtitle2"
+                    color="text.secondary"
+                    style={{ textAlign: "left" }}
+                  >
                     Location: {p.location}
                   </Typography>
-                </CardContent>
-              </Card>{" "}
-            </Link>
-          </Grid>
-        ))
-      }
+                </div>
+
+                <Likes number={p.likes} />
+              </CardContent>
+            </Card>{" "}
+          </Link>
+        </Grid>
+      ))}
     </Grid>
   );
 };
