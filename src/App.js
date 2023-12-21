d2f8f03081cbfed413a39033d56d1de3c7d3c918
Sara Sukovic <sara.sukovic@productdock.com>
2023-12-21 10:15:50 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import {
@@ -11,7 +11,7 @@ import {
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
         <Routes>
           <Route path="/home-page" element={<HomePage />} />
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
+import "./HomePage.css";
 
 export const HomePage = () => {
-  return <div>Home page</div>;
+  return (
+    <Box className="homepage">
+      <Typography variant="h4">Travel experiences</Typography>
+      <Box>Search goes here</Box>
+      <Box>
+        <Typography variant="h4">Most popular posts</Typography>
+      </Box>
+      <Box>
+        <Typography variant="h4">Most asked questions</Typography>
+      </Box>
+    </Box>
+  );
 };
